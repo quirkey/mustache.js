48dc847d1a3f51e765a73830dcc8131093bc8c70
Aaron Quint <aaron@quirkey.com>
2009-10-14 22:08:09 -0400

    What if I want to run all the tests?

--- test/mustache_test.rb
+++ test/mustache_test.rb
@@ -3,32 +3,43 @@
 
 require 'json'
 
-testname = ARGV[0]
-puts "doing #{testname}"
+def run_test_for(testname)
+  puts "doing #{testname}"
 
-view = File.read("examples/#{testname}.js")
-template = File.read("examples/#{testname}.html").to_json
-expect = File.read("examples/#{testname}.txt")
+  view = File.read("examples/#{testname}.js")
+  template = File.read("examples/#{testname}.html").to_json
+  expect = File.read("examples/#{testname}.txt")
 
-mustache = File.read("mustache.js")
-runner = <<-JS
-#{mustache}
-#{view}
-var template = #{template};
-var result = Mustache.to_html(template, #{testname});
-print(result);
-JS
+  mustache = File.read("mustache.js")
+  runner = <<-JS
+  #{mustache}
+  #{view}
+  var template = #{template};
+  var result = Mustache.to_html(template, #{testname});
+  print(result);
+  JS
 
-File.open("runner.js", 'w') {|f| f << runner}
+  File.open("runner.js", 'w') {|f| f << runner}
 
-result = `js runner.js`
+  result = `js runner.js`
 
-if(result == expect)
-  puts "OK"
+  if(result == expect)
+    puts "OK"
+  else
+    puts "Error in #{testname}"
+    puts "Expected"
+    puts "'#{expect}'"
+    puts "Actual"
+    puts "'#{result}'"
+  end
+end
+
+if testname = ARGV.shift
+  run_test_for(testname)
 else
-  puts "Error in #{testname}"
-  puts "Expected"
-  puts "'#{expect}'"
-  puts "Actual"
-  puts "'#{result}'"
+  puts "running all tests"
+  Dir['examples/*.js'].each do |example|
+    testname = File.basename(example, '.js')
+    run_test_for(testname)
+  end
 end
